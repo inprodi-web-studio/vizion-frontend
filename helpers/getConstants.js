b306616b469adbec0a5860f20ba5244b28dfdd97
inprodi-web-studio <proyectos@inprodi.mx>
2024-08-20 11:31:19 -0600

    updated api

--- helpers/getConstants.js
+++ helpers/getConstants.js
@@ -1,7 +1,7 @@
 const getConstants = () => {
     return {
-        API_URL : process.env.NODE_ENV === "development" ? "https://vizion-api-sjqwt.ondigitalocean.app/api" : "https://vizion-api-sjqwt.ondigitalocean.app/api",
-        // API_URL : process.env.NODE_ENV === "development" ? "https://mayfly-intimate-polliwog.ngrok-free.app/api" : "https://vizion-api-sjqwt.ondigitalocean.app/api",
+        API_URL : process.env.NODE_ENV === "development" ? "https://vizion-api-t8wzz.ondigitalocean.app/api" : "https://vizion-api-t8wzz.ondigitalocean.app/api",
+        // API_URL : process.env.NODE_ENV === "development" ? "https://mayfly-intimate-polliwog.ngrok-free.app/api" : "https://vizion-api-t8wzz.ondigitalocean.app/api",
     };
 };
 
