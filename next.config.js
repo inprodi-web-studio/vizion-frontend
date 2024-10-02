9c357bde7a0a809d69322657a34b56ae827a0244
inprodi-web-studio <proyectos@inprodi.mx>
2024-10-01 19:06:49 -0600

    tester

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import("next").NextConfig} */
 
 const withTM = require("next-transpile-modules")([
-  "@xyflow/react",
+  // "@xyflow/react",
   "@xyflow/system",
 ]);
 
 const nextConfig = {
   reactStrictMode: false,
-  modularizeImports: {
-    "@phosphor-icons/react": {
-      transform: "@phosphor-icons/react/{{member}}",
-    },
-  },
+  // modularizeImports: {
+  //   "@phosphor-icons/react": {
+  //     transform: "@phosphor-icons/react/{{member}}",
+  //   },
+  // },
 };
 
 module.exports = withTM(nextConfig);
